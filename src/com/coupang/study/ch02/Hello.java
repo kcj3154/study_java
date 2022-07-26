c65402e64fa1490a734cfe31b8c93ff5cd4e20c6
kcj3154 <kcj0531@naver.com>
2022-07-26 16:20:00 +0900

    220726_java

---
+++ src/com/coupang/study/ch02/Hello.java
@@ -0,0 +1,46 @@
+package com.coupang.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		데이터타입 변수명 = 값;
+//		데이터타입 변수명
+		
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+		
+		int width = 100;
+		String name = "김찬중";
+		
+		String title = "비비고 삼계탕 (3개입)";
+		int price  = 29940;
+		int discountPrice = 24420;
+		boolean like = false;
+		int quantity = 1;
+		String link = "www.coupang.bibigo.com";
+		String basketLink = "www.coupang.basket.com";
+		String purchaseLink = "www.coupang.purchase.com";
+		String arrivalDate = "2022-07-19";
+		
+		System.out.println(name);
+		System.out.println("name 변수에 들어가 있는 값은 : " + name);
+		System.out.println("상품명 : " + title);
+		System.out.println("원가 : " + price);
+		System.out.println("할인가 : " + discountPrice);
+		System.out.println("좋아요 : " + like);
+		System.out.println("수량 : " + quantity);
+		System.out.println("상품링크 : " + link);
+		System.out.println("장바구니 : " + basketLink);
+		System.out.println("구매창 : " + purchaseLink);
+		System.out.println("배송일 : " + arrivalDate);
+		
+		
+		
+		
+		
+	}
+
+}
