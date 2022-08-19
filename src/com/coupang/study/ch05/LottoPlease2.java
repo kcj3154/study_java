4e98fe67c2bed0bfe4af841794669f30653a065c
kcj3154 <kcj0531@naver.com>
2022-08-19 17:33:10 +0900

    20220819

---
+++ src/com/coupang/study/ch05/LottoPlease2.java
@@ -0,0 +1,18 @@
+package com.coupang.study.ch05;
+
+public class LottoPlease2 {
+
+	public static void main(String[] args) {
+		
+		// 랜덤 번호 배열 생성
+		
+		// 랜덤 번호 추출 후 배열에 담기
+		
+		// 랜덤 번호 확인
+		
+		// 오름차순으로 순서 정렬
+		
+		
+	}
+
+}
